e9412ebb48b912b0a42cd6aef6fc9275e0052cd3
MazyK11 <33461811+MazyK11@users.noreply.github.com>
2018-02-04 23:14:17 +0100

    koment 0

--- src/duplicita/Duplicita.java
+++ src/duplicita/Duplicita.java
@@ -20,84 +20,48 @@ public class Duplicita {
     public static void main(String[] args) {
 //      exponent k, pro lehčí manipulaci s velikostí posloupnosti
         int k = 1;
+//      p - pole pro posloupnost
         int p [] = new int [(int) Math.pow(10,k)];
+//      identifikační pomocné pole 
         int[] id = new int [p.length];
         rnd(k,p,id);
-//      výpis pole pro vizuální kontrolu u malé posloupnosti
-//        int p [] ={7,9,9,2,5,2,9,9,2,6};
-//        int id[] = {0,1,2,3,4,5,6,7,8,9};
+//      výpis pole pro vizuální kontrolu u malé posloupnosti        
         System.out.print("Posloupnost\n");        
         for(int i = 0;i<p.length;i++){
             System.out.format("%d ",p[i]);
         }
-        System.out.print("\n");
-        System.out.print("ID\n");
-        for(int i = 0;i<id.length;i++){
-            System.out.format("%d ",id[i]);
-        }
-//        reversesort(p,id);
-//        System.out.print("\n");
-//        System.out.print("ID setříděné\n");
-//        for(int i = 0;i<id.length;i++){
-//            System.out.format("%d ",id[i]);
-//        }
-//        System.out.print("\n");
-//        System.out.print("Posloupnost setříděná\n");
-//        for(int i = 0;i<p.length;i++){
-//            System.out.format("%d ",p[i]);
-//        }
-        
+//      volání metody selectsort
         selectsort(p,id);
-        
-//        System.out.print("\n");
-//        System.out.print("ID setříděné\n");
-//        for(int i = 0;i<id.length;i++){
-//            System.out.format("%d ",id[i]);
-//        }
-//        System.out.print("\n");
-//        System.out.print("Posloupnost setříděná\n");
-//        for(int i = 0;i<p.length;i++){
-//            System.out.format("%d ",p[i]);
-//        }
-        
+//      pole pro čísla bez duplicitních čísel
         int pp [] = new int [p.length];
         int[] idid = new int [id.length];
-        
         int count = hledaniduplicit(p,pp,idid,id);
-        
+//      pole, do kterých se nahrají zkrácené posloupnosti bez duplicit
+//      tím zmizí prázdné hodnoty na konci pole
         int posloupnost[] = new int [p.length-count];
         int idcislo[] = new int [id.length-count];
-        
-//        System.out.print("\n");
-//        System.out.print("Posloupnost bez double\n");
         for(int i = 0;i<posloupnost.length;i++){
             posloupnost[i] = pp[i];
-//            System.out.format("%d ",posloupnost[i]);
-        }
-//        System.out.print("\n");
-//        System.out.print("ID duplicity\n");
-        for(int i = 0;i<idcislo.length;i++){
             idcislo[i] = idid[i];
-//            System.out.format("%d ",idcislo[i]);
         }
-        System.out.print("\n");
-//        reversesort(idcislo,posloupnost);
-//        
-//        System.out.print("\n");
-//        for(int i = 0;i<posloupnost.length;i++){
-//            System.out.format("%d ",posloupnost[i]);
-//        }
+//      volání selectsort s vyměněnými proměnnými
+//      ID čísla vrátí seřazenou posloupnost zpátky do původního stavu bez
+//      duplicitních čísel
         selectsort(idcislo,posloupnost);
+//      výpis pole pro vizuální kontrolu u malé posloupnosti  
         System.out.print("\n");
+        System.out.print("Posloupnost bez duplicitních čísel\n");
         for(int i = 0;i<posloupnost.length;i++){
             System.out.format("%d ",posloupnost[i]);
-        }
+        } 
         System.out.print("\n");
-        System.out.format("Počet odstraněných duplicitních čísel je %d\n",count);
+        System.out.format("Počet odstraněných duplicitních"
+                + " čísel je %d\n",count);
     }
-    /** Metoda pro generování náhodných čísel
+    /** Metoda pro generování náhodných čísel pro posloupnost
      * @param k - exponent
      * @param p - pole reprezentující posloupnost
+     * @param id - identifikační pomocné pole
      */    
     public static void rnd(int k, int p[], int[] id){
         Random numbers = new Random();
@@ -105,28 +69,37 @@ public class Duplicita {
 //          upravitelný interval čísel pro generování 
             p[i] = numbers.nextInt(10);
         }
+//      naplnění pomocného pole
         for(int i = 0;i<id.length;i++){
                 id[i] = i;
         }
     }
-    /** Funkce, která provede algoritmus select sort
-     * @param p - pole
-     * setřídí vstupní pole pomocí hledání minima, které následně vymění s 
-     * prvním prvkem
+    /** Metoda, která vyhledá duplicitní prvky
+     * @param p - pole posloupnosti
+     * @param id - identifikační pomocné pole
+     * @param pp - pole bez duplicitních čísel
+     * @param idid - identifikační pole pro pole pp
+     * vyhledá duplicitní prvky a zbylé prvky zapíše do nového pole
+     * @return vrací počet duplicitních prvků
      */
-    public static int hledaniduplicit(int p[], int[] pp, int idid[], int[] id){
+    public static int hledaniduplicit(int p[], int[] pp, int idid[],
+            int[] id){
         int count = 0;
         int k =1;
+//      první číslo je stejné
         pp[0] = p[0];
         idid[0] = id[0];
         for(int j =0;j < p.length;j++){
             for(int i = 1+j;i < p.length;i++){
                 if(p[j] == p[i]){
+//                  počítadlo duplicitních čísel
                     count++;
+//                  podmínka pro duplicitní čísla na konci posloupnosti
                     if(i == p.length-1){
                         j=p.length;
                     }
                 }
+//              přepisování do nového pole
                 else{
                     pp[k] = p[i];
                     idid[k] = id[i];
@@ -138,10 +111,12 @@ public class Duplicita {
         }
         return count;
     }
-    /** Funkce, která provede algoritmus select sort
-     * @param p - pole
+    /** Metoda, která seřadí posloupnost pomocí principu select sort
+     * @param p - pole posloupnosti
+     * @param id - identifikační pomocné pole
      * setřídí vstupní pole pomocí hledání minima, které následně vymění s 
-     * prvním prvkem
+     * prvním prvkem. Při výměně dochází k identické výměně v identifikačním
+     * poli.
      */
     public static void selectsort(int p[],int[] id){
         int min;
@@ -154,33 +129,18 @@ public class Duplicita {
                    min = p[i];
                    h = i;
                 }
+//              řazení duplicitních prvků podle menšího ID  
                 else if (min == p[i]){
                     if(id[h] > id[i]){
                         min = p[i];
                         h = i;
                     }
-                    
                 }
             }
+//          výměna prvků v obou polích
             int m  = p[j];int n = id[j];
             p[j] = p[h];id[j] = id[h];
-            p[h] = m; id[h] = n; 
-        }
-    }
-    public static void reversesort(int p[],int[] id){
-        int min;
-        int h = 0;
-        for(int j =0;j < p.length;j++){
-            min = p[p.length-1-j];
-            for(int i = p.length-1-j;-1 < i;i--){
-                if(min >= p[i]){
-                   min = p[i];
-                   h = i;
-                }
-            }
-            int m  = p[p.length-1-j];int n = id[p.length-1-j];
-            p[p.length-1-j] = p[h];id[p.length-1-j] = id[h];
-            p[h] = m; id[h] = n; 
+            p[h] = m;id[h] = n; 
         }
     }
 }
